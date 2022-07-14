651e586070cf85f6b46823d5403f2485503ffec6
Ana Luisa <53523588+anaestevam@users.noreply.github.com>
2022-07-13 21:30:11 -0300

    Update inversor.vhd

--- Exercícios/trabalhofinal/ULA/inversor.vhd
+++ Exercícios/trabalhofinal/ULA/inversor.vhd
@@ -1,32 +1,27 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-entity display7segmentos is
-	port(data: in std_logic_vector(3 downto 0); 
---		  s0, s1, s2, s3, s4, s5, s6: out std_logic
-		  saida: out std_logic_vector(6 downto 0));
-end; 
+entity inversor is
+    Port (a: in  STD_LOGIC_VECTOR(15 downto 0);
+          s: out  STD_LOGIC_VECTOR(15 downto 0));
+end;
 
-architecture arq of display7segmentos is
-	signal s: std_logic_vector(6 downto 0);
-	begin
-		with data select s <=
-			"0000001" when "0000", -- '0'
-			"1001111" when "0001", -- '1'
-			"0010010" when "0010", -- '2'
-			"0000110" when "0011", -- '3'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-			"1001100" when "0100", -- '4'
-			"0100100" when "0101", -- '5'
-			"0100000" when "0110", -- '6'
-			"0001111" when "0111", -- '7'
-			"0000000" when "1000", -- '8'
-			"0000100" when "1001", -- '9'
-			"0001000" when "1010", -- 'A'
-			"1100000" when "1011", -- 'B'
-			"0110001" when "1100", -- 'C'
-			"1000010" when "1101", -- 'D'
-			"0110000" when "1110", -- 'E'
-			"0111000" when "1111", -- 'F'
-			"1111111" when others;
-		saida <= s;
+architecture arq of inversor is
+begin
+    s(0) <= not a(0);
+	 s(1) <= not a(1);
+	 s(2) <= not a(2);
+	 s(3) <= not a(3);
+	 s(4) <= not a(4);
+	 s(5) <= not a(5);
+	 s(6) <= not a(6);
+	 s(7) <= not a(7);
+	 s(8) <= not a(8);
+	 s(9) <= not a(9);
+	 s(10) <= not a(10);
+	 s(11) <= not a(11);
+	 s(12) <= not a(12);
+	 s(13) <= not a(13);
+	 s(14) <= not a(14);
+	 s(15) <= not a(15);
 end arq;
